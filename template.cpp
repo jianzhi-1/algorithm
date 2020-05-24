149a607d731ebe441dab3eec9f174c3566548f4d
jianzhi-1 <jianzhiw00@gmail.com>
2020-05-24 12:31:55 +0800

    added template

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i < b; i++)
+typedef pair<ll,ll> pi;
+typedef vector<ll> vi;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+}
