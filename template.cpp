b1da63dcf7cbafbdb923615b8bfdba5a31f402a0
Jianzhi Wang <jianzhiw00@gmail.com>
2020-07-02 00:56:51 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,13 +1,23 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 #define ll long long
 #define F first
 #define S second
 #define PB push_back
 #define MP make_pair
 #define REP(i, a, b) for (int i = a; i < b; i++)
+#define VREP(it, v) for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
 typedef pair<ll,ll> pi;
 typedef vector<ll> vi;
+
+template <typename T>
+using pbds_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename K, typename V>
+using pbds_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
 
 int main(){
 	ios_base::sync_with_stdio(0);
