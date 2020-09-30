f09625ca4748b8e4ab5f4550315e429edd3f8ec0
Jianzhi Wang <jianzhiw00@gmail.com>
2020-09-30 11:06:06 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -4,14 +4,21 @@
 using namespace std;
 using namespace __gnu_pbds;
 #define ll long long
-#define F first
-#define S second
+#define f first
+#define s second
+#define b begin
+#define e end
 #define PB push_back
 #define MP make_pair
+#define LB lower_bound
+#define UB upper_bound
 #define REP(i, a, b) for (int i = a; i < b; i++)
+#define REPD(i, a, b) for (int i = a - 1; i >= b; i--)
 #define VREP(it, v) for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
 typedef pair<ll,ll> pi;
 typedef vector<ll> vi;
+
+const ll MOD = 1000000009;
 
 template <typename T>
 using pbds_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
