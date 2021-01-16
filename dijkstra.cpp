647ba64a380b27f67172028192309ecf42a4fd60
Jianzhi Wang <jianzhiw00@gmail.com>
2021-01-16 10:11:38 +0800

    Create dijkstra.cpp

---
+++ dijkstra.cpp
@@ -0,0 +1,22 @@
+const int N = 200005;
+vector<pi> adjList[N];
+ll dist[N];
+priority_queue<pi, vector<pi>, greater<pi> > pq;
+
+int main(){
+	memset(dist, -1, sizeof(dist));
+	dist[S] = 0;
+	pq.push(MP(0, S));
+	while (!pq.empty()) {
+	    pi cur = pq.top();
+	    pq.pop();
+	    ll x = cur.second, d = cur.first;
+	    if (d > dist[x]) continue;
+	    VREP(it, adjList[x]){
+	        ll nx = it->first, nd = d+it->second;
+	        if (dist[nx] != -1 && dist[nx] <= nd) continue;
+	        dist[nx] = nd;
+	        pq.push(MP(nd, nx));
+	    }
+	}
+}
