367748f5cae1c09447bfa0f8e4dd71da7ca3c2f4
Jianzhi Wang <jianzhiw00@gmail.com>
2021-05-08 11:27:41 +0800

    Create char_input.cpp

---
+++ char_input.cpp
@@ -0,0 +1,7 @@
+int main(){
+    //only compatible with scanf and printf
+    //erase ios_base::sync_with_stdio(0);
+    char c=' ';
+    while (c<'0' || c>'9') c=getchar();
+    int a = c - '0';
+}
