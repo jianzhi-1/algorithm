b951a22252cd0484c0d3fe5d6f9a9968531d992d
Jianzhi Wang <jianzhiw00@gmail.com>
2021-05-20 22:26:32 +0800

    Create bridge.cpp

---
+++ graph-theory/bridge.cpp
@@ -0,0 +1,27 @@
+//WARNING: multiple edges unaccounted for
+
+vector<int> adjList[400005];
+vector<pi> edgeList;
+pi dist[400005]; //MP(preorder, highest preorder that can be reached from vertex)
+
+int c = 0; //preorder idx
+void dfs(int x, int par){
+	dist[x].first = dist[x].second = c;
+	c++;
+	VREP(it, adjList[x]){
+		if (*it == par) continue;
+		if (dist[*it].first != -1){
+			//backward edge
+			backedge.insert(MP(x, *it));
+			dist[x].second = min(dist[x].second, dist[*it].first);
+			continue;
+		}
+		dfs(*it, x);
+		dist[x].second = min(dist[x].second, dist[*it].second);
+	}
+}
+
+int main(){
+    REP(i, 0, n) dist[i] = MP(-1, -1);
+    dfs(0, -1);
+}
