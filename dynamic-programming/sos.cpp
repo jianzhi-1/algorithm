9952e3c8f3657ec9d88d40016937ec4b10b53460
Jianzhi Wang <jianzhiw00@gmail.com>
2021-07-03 00:00:00 +0800

    Update sos.cpp

--- dynamic-programming/sos.cpp
+++ dynamic-programming/sos.cpp
@@ -7,6 +7,7 @@ for (ll mask = 0; mask < pN; mask++){
 	dp[mask][0] = A[mask];
 	REP(i, 0, N){
 		if (mask & (1<<i)) dp[mask][i + 1] = dp[mask][i] + dp[mask^(1<<i)][i];
+		//if (!(mask & (1<<i))) dp[mask][i + 1] = dp[mask][i] + dp[mask^(1<<i)][i];
 		else dp[mask][i + 1] = dp[mask][i];
 	}
 	F[mask] = dp[mask][N];
@@ -17,5 +18,6 @@ REP (i, 0, (1 << N)) F[i] = A[i];
 REP(i, 0, N)
     for (ll mask = 0; mask < pN; mask++){
 	    if (mask & (1<<i)) F[mask] += F[mask^(1<<i)];
+	    //if (!(mask & (1<<i))) F[mask] += F[mask^(1<<i)];
     }
 }
