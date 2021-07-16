b5a2ff1325af248759ce44e2bfca83c78d4feee5
Jianzhi Wang <jianzhiw00@gmail.com>
2021-07-16 15:04:09 +0800

    Create lc-template.cpp

---
+++ lc-template.cpp
@@ -0,0 +1,5 @@
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = (int)a; i < (int)b; i++)
+#define VREP(it, v) for (auto it = v.begin(); it != v.end(); it++)
+typedef pair<int, int> pi;
