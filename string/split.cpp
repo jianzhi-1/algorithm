9adcfa26aa81fa81eb7dc03cb831610c9eb9fa7b
Jianzhi Wang <jianzhiw00@gmail.com>
2021-07-16 15:32:35 +0800

    Update split.cpp

--- string/split.cpp
+++ string/split.cpp
@@ -1,9 +1,9 @@
-vector<string> split(string ss, string delimiter) {
+vector<string> split(string ss, string delimiter){
     size_t s = 0, e, delim_len = delimiter.length();
     string token;
     vector<string> res;
 
-    while ((e = ss.find(delimiter, s)) != string::npos) {
+    while ((e = ss.find(delimiter, s)) != string::npos){
         token = ss.substr(s, e - s);
         s = e + delim_len;
         res.PB(token);
