9b6885fdead32453a2fd85213f2517d251a376b9
Jianzhi Wang <jianzhiw00@gmail.com>
2023-01-06 11:17:11 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -27,8 +27,8 @@ const ll MOD = 1000000009;
 const double PI = acos(-1);
 
 int main(){
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 }
 
 //cout.flush()
