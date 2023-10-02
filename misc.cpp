c4f2e7e5eb2bce29c15e3249a14f3c70f18cd230
Jianzhi Wang <jianzhiw00@gmail.com>
2023-10-02 06:58:54 -0700

    Update misc.cpp

--- misc.cpp
+++ misc.cpp
@@ -2,3 +2,17 @@ stoi() // converts C++ string into integer
 
 #include <string> 
 std::string s = std::to_string(42);
+
+
+// split string
+std::string s = "scott>=tiger>=mushroom";
+std::string delimiter = ">=";
+
+size_t pos = 0;
+std::string token;
+while ((pos = s.find(delimiter)) != std::string::npos) {
+    token = s.substr(0, pos);
+    std::cout << token << std::endl;
+    s.erase(0, pos + delimiter.length());
+}
+std::cout << s << std::endl;
