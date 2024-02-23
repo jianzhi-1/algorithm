4db4f05c164505aba4c60d81cce0e86b61ce31a8
Jianzhi Wang <jianzhiw00@gmail.com>
2024-02-23 12:12:00 -0800

    Create template_short.cpp

---
+++ template_short.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = (int)a; i < (int)b; i++)
+typedef pair<ll,ll> pi;
+const ll MOD = 1000000009;
+const ll INF = 2e18;
+const long double EPS = 1e-9;
+const double PI = acos(-1);
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+}
+//cout.flush()
+//cout << fixed << setprecision(9) << x << endl;
